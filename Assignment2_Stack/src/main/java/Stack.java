3b744697a6ef44ba4bce34b98e972585c0a96a55
albadi <as580021@dal.ca>
2023-06-15 15:28:20 -0300

    Repush Phase1 Stack problem again to follow assignment instructions with the same code for StackTest tests and Stack methods to compile

---
+++ Assignment2_Stack/src/main/java/Stack.java
@@ -0,0 +1,16 @@
+/*
+This is the Stack Class that you have to implement
+the code to pass the test cases
+ */
+public class Stack {
+    public int size() {
+        return 0;
+    }
+    public void push(String input) {
+    }
+    public String pop() throws StackUnderFlowException {
+        return "output";
+    }
+}
+class StackUnderFlowException extends Exception{
+}
