26415addc9619b89ba665ae5b1535fcaa6cc4ce8
albadi <as580021@dal.ca>
2023-06-15 16:17:28 -0300

    Phase2: implement code for methods in Queue class to pass the test cases

--- Assignment2_Queue/src/main/java/Queue.java
+++ Assignment2_Queue/src/main/java/Queue.java
@@ -1,14 +1,30 @@
+import java.util.ArrayList;
+
 //Empty classes amd methods made to compile tests, remove when implementation starts
 public class Queue {
-    public int size(){ return 0;}
-
-    public void enqueue(String element){}
-
-    public void dequeue() throws UnderFlowException{}
-
-    public String peek(){return "";}
+    ArrayList myQueue = new ArrayList<>();
+    String dequeuedItem;//This string is to save the dequeued item which always is the first item
+    public int size(){
+        return myQueue.size();
+    }
+    public void enqueue(String element){
+        myQueue.add(element);
+    }
+    public void dequeue() throws UnderFlowException{
+        if (myQueue.isEmpty()){
+            throw new UnderFlowException();
+        }
+        dequeuedItem = myQueue.remove(0).toString();
+    }
+    public String peek(){
+        return dequeuedItem;
+    }
 
 }
 
-class UnderFlowException extends Exception{}
+class UnderFlowException extends Exception{
+    public UnderFlowException(){
+        super("You cannot dequeue from an empty queue.");
+    }
+}
 
